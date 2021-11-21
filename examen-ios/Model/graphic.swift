c29323d844434113f5f929a8308e0bec5b3ae520
PONCE FLORES, CARLOS ANDRES <carlos.ponce@afirma.solutions>
2021-11-21 17:57:56 -0600

    Create models and order proyect

---
+++ examen-ios/Model/graphic.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  examen-ios
+//
+//  Created by Carlos Ponce on 20/11/21.
+//
+
+import Foundation
+
---
+++ examen-ios/View/Components/TextFieldRowView.swift
@@ -0,0 +1,20 @@
+//
+//  TextFieldRowView.swift
+//  examen-ios
+//
+//  Created by Carlos Ponce on 21/11/21.
+//
+
+import SwiftUI
+
+struct TextFieldRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextFieldRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextFieldRowView()
+    }
+}
