d1966ef31aa3960653ea57811eb13b5b2d017ec1
PONCE FLORES, CARLOS ANDRES <carlos.ponce@afirma.solutions>
2021-11-21 17:59:30 -0600

    Integrate Textfield with function to filter

--- examen-ios/Model/graphic.swift
+++ examen-ios/Model/graphic.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  graphic.swift
 //  examen-ios
 //
 //  Created by Carlos Ponce on 20/11/21.
@@ -7,3 +7,21 @@
 
 import Foundation
 
+// MARK: - Welcome
+struct graphic: Codable {
+    let colors: [String]
+    let questions: [Question]
+}
+
+// MARK: - Question
+struct Question: Codable {
+    let total: Int
+    let text: String
+    let chartData: [ChartDatum]
+}
+
+// MARK: - ChartDatum
+struct ChartDatum: Codable {
+    let text: String
+    let percetnage: Int
+}
--- examen-ios/View/Components/TextFieldRowView.swift
+++ examen-ios/View/Components/TextFieldRowView.swift
@@ -6,15 +6,41 @@
 //
 
 import SwiftUI
+import Combine
 
 struct TextFieldRowView: View {
+    @State var name = ""
+    @State var isTextFieldFocused = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TextField("Nombre", text: $name, onEditingChanged: { isEditing in
+                isTextFieldFocused = isEditing
+        })
+            .foregroundColor(isTextFieldFocused ? Color.black : Color.gray)
+            .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
+            .cornerRadius(5)
+            .overlay(RoundedRectangle(cornerRadius: 5)
+                    .stroke(lineWidth: 0.5))
+            .onReceive(Just(self.name)) { newValue in
+                self.name.alphanumericValidator(newValue: newValue)
+            }
+    }
+}
+
+extension String {
+    mutating func alphanumericValidator(newValue: String) {
+        let alphabet = "abcdefghijklmnñopqrstuvwxyz"
+        let numbers = "1234567890"
+        let alphanumericValue = alphabet + alphabet.uppercased() + numbers
+        let filter = newValue.filter {alphanumericValue.contains($0)}
+        if filter != newValue {
+            self = filter
+        }
     }
 }
 
 struct TextFieldRowView_Previews: PreviewProvider {
     static var previews: some View {
         TextFieldRowView()
+            .previewLayout(.fixed(width: 350.0, height: 100))
     }
 }
