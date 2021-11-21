e8bd92fcb6acb72f67b7b2c2378541b012164e87
PONCE FLORES, CARLOS ANDRES <carlos.ponce@afirma.solutions>
2021-11-20 21:00:11 -0600

    Initial Commit

---
+++ examen-ios/examen_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  examen_iosApp.swift
+//  examen-ios
+//
+//  Created by Carlos Ponce on 20/11/21.
+//
+
+import SwiftUI
+
+@main
+struct examen_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
