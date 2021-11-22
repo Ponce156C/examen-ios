eea54eea93574da1431a7cc2f5c5980159c64843
PONCE FLORES, CARLOS ANDRES <carlos.ponce@afirma.solutions>
2021-11-22 01:27:51 -0600

    Modify mainView to present partial sheet with option to image and append SourceTypeSelectView

--- examen-ios/View/ContentView.swift
+++ examen-ios/View/ContentView.swift
@@ -7,27 +7,41 @@
 
 import SwiftUI
 import UIKit
+import PartialSheet
 
 struct ContentView: View {
-    @State private var isShowPhotoLibrary = false
+    @State private var isCamera = false
+    @State private var isLibrary = false
     @State private var userImage: UIImage = UIImage()
+    @EnvironmentObject var partialSheetManager : PartialSheetManager
     
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            VStack (spacing:10) {
-                TextFieldRowView(name: "")
-                Button(action: {
-                    self.isShowPhotoLibrary = true
-                }) {
-                    ImageRowView(ImageSelected: self.$userImage)
-                }
-            }.padding()
-                .sheet(isPresented: $isShowPhotoLibrary) {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack (spacing:10) {
+                    TextFieldRowView(name: "")
+                    Button(action: {
+                        self.partialSheetManager.showPartialSheet {
+                            SourceTypeSelectView(isLibrary: $isLibrary, isCamera: $isCamera)
+                        }
+                    }) {
+                        ImageRowView(ImageSelected: self.$userImage)
+                    }
                     
-                    ImagePicker(sourceType: .photoLibrary, selectedImage: self.$userImage)
+                    Spacer()
                 }
+                .padding(.horizontal)
+                .fullScreenCover(isPresented: $isLibrary, content: {
+                    ImagePicker(sourceType: .photoLibrary, partialSheetManager: _partialSheetManager, selectedImage: self.$userImage)
+                })
+                .fullScreenCover(isPresented: $isCamera, content: {
+                    ImagePicker(sourceType: .camera, partialSheetManager: _partialSheetManager, selectedImage: self.$userImage)
+                })
+            }
+            .navigationBarTitle("Examen - ios")
         }
-        
+        .addPartialSheet(style: .defaultStyle())
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
