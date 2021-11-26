f2b82e65344eaac5b04baaded888ad9d042bea90
PONCE FLORES, CARLOS ANDRES <carlos.ponce@afirma.solutions>
2021-11-26 01:40:28 -0600

    Create documents utilities

---
+++ examen-ios/View/Components/PieChartView.swift
@@ -0,0 +1,20 @@
+//
+//  PieChartView.swift
+//  examen-ios
+//
+//  Created by Carlos Ponce on 22/11/21.
+//
+
+import SwiftUI
+
+struct PieChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PieChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        PieChartView()
+    }
+}
